774ac587734d0dcedd5769b06fc65d1691a3d482
Minombreesvera <kehindebolaji25@gmail.com>
2023-09-25 23:20:56 +0100

    my third attempt of coding

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints all possible combinations
+ * Return: 0
+ */
+int main()
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		 putchar(i);
+		 putchar(',');
+		 putchar(' ');
+	}
+		putchar('\n');
+		return 0;
+}
