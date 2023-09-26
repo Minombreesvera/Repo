253dacb4ba1b30ee6debc0a532dad092a33a26fd
Minombreesvera <kehindebolaji25@gmail.com>
2023-09-26 12:25:49 +0100

    my third attempt in coding

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - To print the alphabets in lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char vera;
+
+		for (vera = 'a'; vera <= 'z'; vera++)
+		{
+		_putchar(vera);
+		_putchar('\n');
+		}
+		return (0);
+}
